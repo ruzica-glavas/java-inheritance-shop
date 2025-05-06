6b2493a1914a920421c55e83e1bead143370003e
ruzica-glavas <ruzi99@hotmail.it>
2025-05-06 16:51:43 +0200

    add: cuffie

--- src/org/lessons/java/shop/Cuffie.java
+++ src/org/lessons/java/shop/Cuffie.java
@@ -1,5 +1,32 @@
 package org.lessons.java.shop;
 
-public class Cuffie {
+import java.math.BigDecimal;
 
+public class Cuffie extends Prodotto {
+    private String color;
+    private boolean isWireless;
+
+    public Cuffie(String name, String brand, BigDecimal price, int iva, String color, boolean isWireless){
+        super(name, brand, price, iva);
+        this.color=color;
+        this.isWireless=isWireless;
+        
+    }
+
+    //gestione dei getter e setter
+    public String getColor(){
+        return this.color;
+    }
+
+    public void setColor(String color){
+        this.color=color;
+    }
+
+    public boolean getIsWireless(){
+        return this.isWireless;
+    }
+
+    public void setIsWireless(boolean isWireless){
+        this.isWireless=isWireless;
+    }
 }
--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -5,13 +5,17 @@ import java.math.RoundingMode;
 
 public class Main {
     public static void main(String[] args) {
-        BigDecimal decimal = new BigDecimal(100).setScale(2, RoundingMode.FLOOR);
-        Prodotto p = new Prodotto("Ciao", "Gucci",decimal, 15);
-        System.out.println(p.getPrice());
-        System.out.println(p.getCode());
+        BigDecimal decimalApple = new BigDecimal(1020.90).setScale(2, RoundingMode.FLOOR);
+        BigDecimal decimalSamsung = new BigDecimal(500.20).setScale(2, RoundingMode.FLOOR);
+        BigDecimal decimalJbl = new BigDecimal(49.99).setScale(2, RoundingMode.FLOOR);
+
+
+        //Prodotto p = new Prodotto("Ciao", "Gucci",decimalApple, 15);
+        //System.out.println(p.getPrice());
+        //System.out.println(p.getCode());
 
         //Prodotto smartphone
-        Smartphone apple= new Smartphone("Iphone 14", "Apple", decimal, 22, 64);
+        Smartphone apple= new Smartphone("Iphone 14", "Apple", decimalApple, 22, 64);
         System.out.println("Nome prodotto:" + apple.getName());
         System.out.println("Brand:" + apple.getBrand());
         System.out.println("Codice prodotto:" + apple.getCode());
@@ -22,19 +26,27 @@ public class Main {
         System.out.println(apple.getMemory() + "GB di memoria");
 
         //Prodotto televisore
-        Televisore samsung= new Televisore("Smart TV", "Samsung", decimal, 22, 100, true);
+        Televisore samsung= new Televisore("Smart TV", "Samsung", decimalSamsung, 22, 100, true);
 
         System.out.println("Il nome del prodotto:" + samsung.getName());
         System.out.println("Il nome del brand:" + samsung.getBrand());
         System.out.println("Il codice del prodotto:" + samsung.getCode());
         System.out.println("Il prezzo del prodotto:" + samsung.getPrice());
-        System.out.println("L'iva:" + samsung.getIva());
+        System.out.println("L'iva:" + samsung.getIva() + "%");
         System.out.println("Il prezzo con iva inclusa:" + samsung.getPriceIva());
         System.out.println("Le dimensioni sono:" + samsung.getSize() + " " + "pollici");
         System.out.println("È una smart tv:" + samsung.getIsSmart());
 
+        //Prodotto cuffie
+        Cuffie jbl = new Cuffie("JBL TUNE 510BT", "JBL", decimalJbl, 22, "Blu", true);
 
-
-
+        System.out.println("Nome prodotto:" + jbl.getName());
+        System.out.println("Il nome del brand:" + jbl.getBrand());
+        System.out.println("Il codice del prodotto:" + jbl.getCode());
+        System.out.println("Il prezzo del prodotto:" + jbl.getPrice());
+        System.out.println("L'iva:" + jbl.getIva() + "%");
+        System.out.println("Il prezzo con iva inclusa:" + jbl.getPriceIva());
+        System.out.println("Colore:" + jbl.getColor());
+        System.out.println("Wireless:" + jbl.getIsWireless());
     }
 }
